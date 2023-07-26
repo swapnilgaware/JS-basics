efe92db2fa0a2073517c435fea9ffc7b99e2c401
swapnilgaware <gawareswapnil85@gmail.com>
2023-07-26 23:44:07 +0530

    asynch program

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ var user=[
     {name:"Ashu", age:26}
 ];
 
-console.log(user[1]);
+//console.log(user[1]);
+
+function printToScreen(){
+    console.log("Helloworld");
+}
+
+setTimeout(printToScreen, 1 * 1000);
+
+let counter =0;
+for (let i=0;i<1000; i++){
+    counter= counter + 1;
+}
+console.log(counter);
